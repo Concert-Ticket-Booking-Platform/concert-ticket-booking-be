fa2554ba7c528b8ca854df144cb5a02ff53404f1
Công Thành <dct01112004@gmail.com>
2026-09-09 23:55:12 +0700

    add momo dto

---
+++ src/ConcertTicket.Application/Payments/DTOs/MoMoCreatePaymentRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace ConcertTicket.Application.Payments.DTOs;
+
+public sealed record MoMoCreatePaymentRequest(
+    string PartnerCode,
+    string RequestType,
+    string IpnUrl,
+    string RedirectUrl,
+    string OrderId,
+    long Amount,
+    string OrderInfo,
+    string RequestId,
+    string ExtraData,
+    string Lang,
+    string Signature);
---
+++ src/ConcertTicket.Application/Payments/DTOs/MoMoCreatePaymentResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace ConcertTicket.Application.Payments.DTOs;
+
+public sealed record MoMoCreatePaymentResponse(
+    string PartnerCode,
+    string RequestId,
+    string OrderId,
+    long Amount,
+    long ResponseTime,
+    string Message,
+    int ResultCode,
+    string? PayUrl,
+    string? Deeplink,
+    string? QrCodeUrl,
+    string? Signature);
